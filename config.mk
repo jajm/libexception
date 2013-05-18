9eba4018bcee981e64e64193091443f41c0d22eb
Julian Maurice <jajm@jajm.org>
2013-05-18 12:16:38 +0200

    Split config.mk in two parts
    config.mk is for user's configuration
    package.mk contains informations about the library

--- config.mk
+++ config.mk
@@ -7,7 +7,3 @@ PREFIX := /usr/local
 LIBDIR := $(PREFIX)/lib
 INCLUDEDIR := $(PREFIX)/include/libexception
 PKGCONFIGDIR := $(LIBDIR)/pkgconfig
-
-LIBTOOL_CURRENT := 0
-LIBTOOL_REVISION := 0
-LIBTOOL_AGE := 0
