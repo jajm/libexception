14f6813bc17c60c059e6c516fa55864a8d9106ac
Julian Maurice <jajm@jajm.org>
2013-09-25 07:30:36 +0200

    Use autoconf to build a valid pkgconfig file

--- config.mk
+++
@@ -1,9 +0,0 @@
-CC := gcc
-MAKE := make
-LIBTOOL := libtool
-INSTALL := install
-
-PREFIX := /usr/local
-LIBDIR := $(PREFIX)/lib
-INCLUDEDIR := $(PREFIX)/include/libexception
-PKGCONFIGDIR := $(LIBDIR)/pkgconfig
